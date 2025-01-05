af14e6ac94c8206063047928848718b86458fd3a
YourParalysisDemon <145605048+YourParalysisDemon@users.noreply.github.com>
2025-01-04 23:26:56 -0500

    Update Pycho.py
    Added walk speed function and x,y,z coords for future stuff.

--- Pycho.py
+++ Pycho.py
@@ -17,7 +17,6 @@ while True:
     else:
         print("Try again retard")
 
-
 mem = Pymem("Psychonauts")
 
 module1 = module_from_name(mem.process_handle, "Psychonauts.exe").lpBaseOfDll
@@ -27,6 +26,10 @@ gravity1_offsets = [0X0, 0X94]
 blast_offsets = [0X4, 0X18, 0XC7, 0X253C]
 run_offsets = [0X4, 0X708]
 no_clip_offsets = [0X0, 0X110]
+walk_offsets = [0X120]  # 003839D8
+z_offsets = [0X44, 0X10]  # 003839D8
+y_offsets = [0X0, 0X40]  # 00386AE0
+x_offsets = [0X10, 0X48]  # 003839D8
 
 
 def getpointeraddress(base, offsets):
@@ -45,6 +48,11 @@ def multi_run_god():
 
 def multi_run_move():
     new_thread = Thread(target=fuck_walking, daemon=True)
+    new_thread.start()
+
+
+def multi_run_move2():
+    new_thread = Thread(target=fuck_walking2, daemon=True)
     new_thread.start()
 
 
@@ -128,6 +136,18 @@ def fuck_walking():
             break
 
 
+def fuck_walking2():
+    addr = getpointeraddress(module1 + 0x003839D8, walk_offsets)
+    while 1:
+        try:
+            mem.write_int(addr, 0x40c00000)
+        except pymem.exception.MemoryWriteError as e:
+            print(f"Error writing memory: {e}")
+        if keyboard.is_pressed("C"):
+            mem.write_int(addr, 0x3f800000)
+            break
+
+
 pygame.init()
 pygame.mixer_music.load("music/mod.mp3")
 pygame.mixer_music.play(1)
@@ -138,7 +158,7 @@ root.wm_iconphoto(False, photo)
 root.attributes("-topmost", True)
 root.title("Fragging Terminal")
 root.configure(background='dark red')
-root.geometry("265x180")
+root.geometry("265x200")
 
 
 def callback(url):
@@ -159,10 +179,12 @@ button2 = tk.Button(root, text="Fuck Gravity", bg='black', fg='white', command=m
 button2.grid(row=2, column=0)
 button3 = tk.Button(root, text="Fuck Walking", bg='black', fg='white', command=multi_run_move)
 button3.grid(row=3, column=0)
+button3 = tk.Button(root, text="Fuck Walking 2", bg='black', fg='white', command=multi_run_move2)
+button3.grid(row=4, column=0)
 button4 = tk.Button(root, text="Legendary Mode", bg='black', fg='white', command=multi_run_legendary)
-button4.grid(row=4, column=0)
+button4.grid(row=5, column=0)
 button5 = tk.Button(root, text="Exit", bg='white', fg='black', command=root.destroy)
-button5.grid(row=5, column=0)
+button5.grid(row=6, column=0)
 label1 = tk.Label(master=root, text='C Show GUI', bg='red', fg='black')
 label1.grid(row=0, column=3)
 label2 = tk.Label(master=root, text='V Hide GUI', bg='red', fg='black')
@@ -180,9 +202,8 @@ label8.grid(row=5, column=3)
 label9 = tk.Label(master=root, text='G Fuck Walls', bg='red', fg='black')
 label9.grid(row=6, column=3)
 link1 = tk.Label(root, text="Your Sleep Paralysis Demon", bg="black", fg="red", cursor="hand2")
-link1.grid(row=6, column=0)
+link1.grid(row=7, column=0)
 link1.bind("<Button-1>", lambda e: callback("https://steamcommunity.com/profiles/76561198259829950/"))
-
 
 keyboard.add_hotkey("c", show)
 keyboard.add_hotkey("v", hide)
